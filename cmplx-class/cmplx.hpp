599b55d1d3ddd6c219db8cecfac5a3446357f133
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-02-06 11:01:57 +0530

    cmplx.hpp

--- cmplx-class/cmplx.hpp
+++ cmplx-class/cmplx.hpp
@@ -1,15 +1,26 @@
-#include<iostream>
- class cmplx     
-{
- public:
-   double real;
-   double img;
-   
-public :
-    cmplx();           //default constructor
-   void display();
- cmplx add(cmplx y);       //function call 
- cmplx sub(cmplx y);       //function call 
- cmplx mult(cmplx y);
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
 
+#include <iostream>
+#include <cmath> // For norm calculation
+
+class Complex {
+public:
+    double real, img;
+
+    // Constructors
+    Complex();
+    Complex(double r, double i);
+
+    // Basic operations
+    Complex add(const Complex& other);
+    Complex sub(const Complex& other);
+    Complex mult(const Complex& other);
+    Complex conjugate();
+    double norm();
+
+    // Display function
+    void display();
 };
+
+#endif
