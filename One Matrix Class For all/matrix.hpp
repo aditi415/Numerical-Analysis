5656a47140fe815a86e4b36e6dfa4ca76a8b2150
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-03-07 13:11:08 +0530

    Add files via upload

---
+++ One Matrix Class For all/matrix.hpp
@@ -0,0 +1,34 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <fstream>
+
+class Matrix {
+private:
+    std::vector<std::vector<double>> data;
+    int rows, cols;
+    void pivot(int row);
+    void backSubstitution(std::vector<double>& solution);
+
+public:
+    Matrix(int r, int c);
+    Matrix(const std::string& filename);
+
+    void readFromFile(const std::string& filename);
+    void writeToFile(const std::string& filename) const;
+    
+    Matrix operator+(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
+    bool isSymmetric() const;
+    bool isIdentity() const;
+    void print() const;
+    
+    void gaussianElimination(std::vector<double>& solution);
+};
+
+#endif // MATRIX_HPP
