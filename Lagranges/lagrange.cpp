ec5dbae331f0f25297ec0b005e2979c111868c07
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-04-30 12:39:11 +0530

    lagrange.cpp

--- Lagranges/lagrange.cpp
+++ Lagranges/lagrange.cpp
@@ -1,17 +1,32 @@
-#include "lagrange.hpp"
+#include "lagrange.h"
 
-double lagrangeInterpolation(double x, double x_vals[], double y_vals[], int n) {
-    double result = 0.0;
+Lagrange::Lagrange(int size) {
+    n = size;
+    x_vals = new double[n];
+    y_vals = new double[n];
+}
 
-    for (int i = 0; i < n; i++) {
+Lagrange::~Lagrange() {
+    delete[] x_vals;
+    delete[] y_vals;
+}
+
+void Lagrange::setData(double x_array[], double y_array[]) {
+    for (int i = 0; i < n; ++i) {
+        x_vals[i] = x_array[i];
+        y_vals[i] = y_array[i];
+    }
+}
+
+double Lagrange::interpolate(double x) {
+    double result = 0;
+    for (int i = 0; i < n; ++i) {
         double term = y_vals[i];
-        for (int j = 0; j < n; j++) {
-            if (j != i) {
+        for (int j = 0; j < n; ++j) {
+            if (j != i)
                 term *= (x - x_vals[j]) / (x_vals[i] - x_vals[j]);
-            }
         }
         result += term;
     }
-
     return result;
 }
