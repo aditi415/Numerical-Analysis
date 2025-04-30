99043be45106774c5a6202796d0dc0699b60c586
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-04-30 12:38:23 +0530

    lagrange.hpp

--- Lagranges/lagrange.hpp
+++ Lagranges/lagrange.hpp
@@ -1,7 +1,18 @@
 #ifndef LAGRANGE_HPP
 #define LAGRANGE_HPP
 
-double lagrangeInterpolation(double x, double x_vals[], double y_vals[], int n);
+class Lagrange {
+private:
+    double* x_vals;
+    double* y_vals;
+    int n;
+
+public:
+    Lagrange(int size);
+    ~Lagrange();
+
+    void setData(double x_array[], double y_array[]);
+    double interpolate(double x);
+};
 
 #endif
-
