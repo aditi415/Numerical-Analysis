6d0902d936486c99c6fe28169231b55f7d13c462
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-05-02 22:01:10 +0530

    matrix.hpp

--- One Matrix Class For all/matrix.hpp
+++ One Matrix Class For all/matrix.hpp
@@ -12,6 +12,7 @@ class Matrix {
 private:
     std::vector<std::vector<double>> data;
     int rows, cols;
+
     void pivot(int row);
     void backSubstitution(std::vector<double>& solution);
 
@@ -21,14 +22,17 @@ public:
 
     void readFromFile(const std::string& filename);
     void writeToFile(const std::string& filename) const;
-    
+
     Matrix operator+(const Matrix& other) const;
     Matrix operator-(const Matrix& other) const;
+
     bool isSymmetric() const;
     bool isIdentity() const;
-    void print() const;
-    
+    bool isDiagonallyDominant() const;
+    void makeDiagonallyDominant();
+
     void gaussianElimination(std::vector<double>& solution);
+    void print() const;
 };
 
 #endif // MATRIX_HPP
